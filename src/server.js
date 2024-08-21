795c33b73dccb72cdca545b594d0fc9d6ae5af6e
project_Html <khoanguyenduc99@gmail.com>
2024-08-21 10:59:48 +0700

    use MongoDB driver

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
 
 const connection = require('./configuration/database.js')
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 8081;
@@ -31,7 +32,29 @@ app.use('/v1/api/',apiroutes);
 (async() => {
     try{
         //test connection
-        await connection();
+        //using mongoose
+        // await connection();
+
+        // using mongodb driver
+        // Connection URL
+        const url = process.env.DB_HOST_WITH_DRIVER;
+        const client = new MongoClient(url);
+
+        // Database Name
+        const dbName = process.env.DB_NAME;
+
+        await client.connect();
+        console.log('Connected successfully to server');
+        const db = client.db(dbName);
+        const collection = db.collection('customers');
+
+        // await collection.insertOne({
+        //     "name": "Khoa"
+        // })
+        // collection.insertOne({"address": "hcm", email: "khoanguyenduc99@gmail.com"});
+        // collection.insertOne({array: [1,2,3]});
+        const a = await collection.findOne({address: "dalat"});
+        console.log(">>> find data: ", a);
         app.listen(port,hostname, () => {
             console.log(`Backend zero app listening on port ${port}`);
         })
